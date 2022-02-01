182d0d728eb64c36f6cae9ab939b04058f117767
aabagdi <70984836+aabagdi@users.noreply.github.com>
2022-01-31 19:29:20 -0800

    Add files via upload

--- Question5.c
+++ Question5.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int randomMean(int seed, int length) {
-    srand(seed);
-    int randTotal;
-    for (int i = 0; i <= length; i++) {
-        auto randInt = rand();
-        printf(randInt, "%d");
+    srand(time(NULL));
+    int randTotal = 0;
+    for(int i = 0; i < length; i++) {
+        int randInt = rand() % seed;
+        printf(" %u ", randInt);
         randTotal = randTotal + randInt;
     }
-    return (randTotal/length);
+    printf("\n");
+    return randTotal / length;
 }
 
 int main() {
     int userSeed;
     int userLength;
     printf("What is your seed? ");
-    scanf("%d", &userSeed);
-    printf("How many numbers do you want to use? ")
-    scanf("%d", &userLength);
-    auto result = randomMean(userSeed, userLength);
-    printf("Your random mean is: ", result);
+    scanf("%u", &userSeed);
+    printf("How many numbers do you want to use? ");
+    scanf("%u", &userLength);
+    int result = randomMean(userSeed, userLength);
+    printf("Your random mean is: %u ", result);
     return 0;
 }
